6a41b2a53181038b7d2b4f092f54bbb1d306138b
Gustavo Cesar <gustavosommer123@gmail.com>
2026-03-04 23:18:08 -0300

    Exercicio 19

--- ListaExercicios/ListasDeExercicios/src/lista1/EXC6.java
+++ ListaExercicios/ListasDeExercicios/src/lista1/EXC6.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EXc6 {
+public class EXC6 {
 
     public static void main(String[] args){
 
--- ListaExercicios/ListasDeExercicios/src/lista2/EXC19.java
+++ ListaExercicios/ListasDeExercicios/src/lista2/EXC19.java
@@ -1,4 +1,34 @@
 package lista2;
 
+import java.util.Scanner;
+
 public class EXC19 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe sua Pressão Arterial Diástolica (PAD)");
+        double pad = sc.nextDouble();
+
+        System.out.println("Informe sua Pressão Arterial Sistólica(PAS)");
+        double pas = sc.nextDouble();
+
+        if (pad < 85 && pas < 130){
+            System.out.println("Pressão arterial normal");
+        } else if ((pad >= 85 && pad <= 89) || (pas >= 130 && pas <= 139)) {
+            System.out.println("Pressão arterial normal limítrofe");
+        }else if ((pad >= 90 && pad <= 99) || (pas >= 140 && pas <= 159)){
+            System.out.println("Hípertensão leve(Estágio 1");
+        }else if ((pad >= 100 && pad <= 109) || (pas >= 160 && pas <= 179)){
+            System.out.println("Hípertensão moderada(Estágio 2)");
+        }else if (pad >= 110 && pas >= 180){
+            System.out.println("Hípertensão grave(Estágio 3)");
+        } else if (pad < 90 && pas >= 140) {
+            System.out.println("Hípertensão sistólica isolada");
+        }
+
+        sc.close();
+    }
+
 }
