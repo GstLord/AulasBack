ccb4dfe1f13a850a6e7ecd5d8e1b72bf8fee9480
GustavoCesar <gustavosommer123@gmail.com>
2026-09-02 12:31:59 -0300

    att

--- trabalhoModulo13/src/Alunos/Alunos.java
+++ trabalhoModulo13/src/Alunos/Alunos.java
@@ -7,14 +7,64 @@ public class Alunos {
     private int[] nota;
     private int numeroProvas;
 
-
     public Alunos(String nome, String turma, int numeroProvas) {
         this.nome = nome;
         this.turma = turma;
         this.numeroProvas = numeroProvas;
+        this.nota = new int[numeroProvas];
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
     }
 
     public int getNumeroProvas() {
         return numeroProvas;
+    }
+
+    public int getNota(int indice) {
+        return nota[indice];
+    }
+
+    public boolean setNota(int indice, int valor) {
+        if (valor < 0 || valor > 10) {
+            return false;
+        }
+        this.nota[indice] = valor;
+        return true;
+    }
+
+    public double calcularMedia() {
+        int soma = 0;
+        for (int i = 0; i < numeroProvas; i++) {
+            soma += nota[i];
+        }
+        return (double) soma / numeroProvas;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder notas = new StringBuilder();
+        for (int i = 0; i < numeroProvas; i++) {
+            notas.append(nota[i]);
+            if (i < numeroProvas - 1) {
+                notas.append(", ");
+            }
+        }
+        return "Aluno: " + nome + " | Turma: " + turma +
+                " | Notas: [" + notas + "]" +
+                " | Media: " + String.format("%.2f", calcularMedia());
     }
 }
--- trabalhoModulo13/src/TrabalhoModulo13.java
+++ trabalhoModulo13/src/TrabalhoModulo13.java
@@ -1,3 +1,7 @@
+import Alunos.Alunos;
+
+import java.util.Scanner;
+
 public class TrabalhoModulo13 {
 /*
 Crie o objeto Aluno, com atributos e métodos de sua preferência.
@@ -12,8 +16,48 @@ Crie cada aluno com parâmetros de construtores de sua escolha. O objeto aluno d
 
 Não permita que um aluno tenha uma nota menor do que zero ou maior do que 10. Informe o usuário que ele digitou uma nota inválida e precisa repreencher a nota em caso de invalidez.
 */
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Quantos alunos serão cadastrados? ");
+        int quantidadeAlunos = scanner.nextInt();
 
+        System.out.print("Quantas provas cada aluno fez no ano? ");
+        int quantidadeProvas = scanner.nextInt();
+
+        Alunos[] alunos = new Alunos[quantidadeAlunos];
+
+        for (int i = 0; i < quantidadeAlunos; i++) {
+            System.out.println("\n--- Cadastro do aluno " + (i + 1) + " ---");
+
+            System.out.print("Nome: ");
+            String nome = scanner.next();
+
+            System.out.print("Turma: ");
+            String turma = scanner.next();
+
+            Alunos aluno = new Alunos(nome, turma, quantidadeProvas);
+
+            for (int j = 0; j < quantidadeProvas; j++) {
+                boolean notaValida = false;
+                while (!notaValida) {
+                    System.out.print("Nota da prova " + (j + 1) + ": ");
+                    int nota = scanner.nextInt();
+                    notaValida = aluno.setNota(j, nota);
+                    if (!notaValida) {
+                        System.out.println("Nota inválida! Digite um valor entre 0 e 10.");
+                    }
+                }
+            }
+
+            alunos[i] = aluno;
+        }
+
+        System.out.println("\n--- Resultado final ---");
+        for (Alunos aluno : alunos) {
+            System.out.println(aluno);
+        }
+
+        scanner.close();
     }
 }
